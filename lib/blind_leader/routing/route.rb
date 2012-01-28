729c35b9f8296ddd9cac06f5269aac14e0d48a93
Lex Childs <lexchilds@gmail.com>
2012-01-28 09:35:17 -0600

    Route responds to verbs

--- lib/blind_leader/routing/route.rb
+++ lib/blind_leader/routing/route.rb
@@ -1,5 +1,22 @@
 module BlindLeader
   class Routing::Route < Proc
+
+
+    def klass
+    end
+
+    def get
+    end
+
+    def post
+    end
+
+    def put
+    end
+
+    def delete
+    end
+
   end
 end
 
--- spec/route_spec.rb
+++ spec/route_spec.rb
@@ -3,26 +3,26 @@ require 'helper'
 describe 'Route' do
   
     describe 'Methods' do
-      subject { BlindLeader::Routing::Route.new }
+      subject { BlindLeader::Routing::Route.new {|| 'lego' }  }
 
       it 'has "klass"' do
-        subject.must_respond_to :klass
+        subject.must_respond_to subject, :klass
       end
 
       it 'has "get"' do
-        subject.must_respond_to :get
+        subject.must_respond_to subject, :get
       end
 
       it 'has "post"' do
-        subject.must_respond_to :post
+        subject.must_respond_to subject, :post
       end
 
       it 'has "put"' do
-        subject.must_respond_to :put
+        subject.must_respond_to subject, :put
       end
 
       it 'has "delete"' do
-        subject.must_respond_to :delete
+        subject.must_respond_to subject, :delete
       end
 
     end
